6a89bd1318fae28cece381a50e87d54e51abc1a7
Simon Descarries <40470761+sdescarries@users.noreply.github.com>
2021-11-11 06:34:54 -0500

    fixup

--- src/Password/Password.tsx
+++ src/Password/Password.tsx
@@ -1,11 +1,20 @@
 import { pwgen } from './pwgenFactory';
 import { usePassword } from './usePassword';
 
+export interface FieldProps {
+  word: string;
+}
+
+export const Field = ({ word }: FieldProps) => 
+  (
+    <pre>{word}</pre>
+  );
+
 export interface PasswordProps {
   generator?: pwgen;
 }
 
 export function Password({ generator }: PasswordProps): JSX.Element {
   const value = usePassword(generator);
-  return <pre>{value}</pre>;
+  return <Field word={value} />;
 }
