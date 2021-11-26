0e085984c66c4c196e42aa3e626426eb4c8ba21f
Simon Descarries <40470761+sdescarries@users.noreply.github.com>
2021-11-26 09:13:51 -0500

    fixup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,35 @@
 module.exports = {
-  'env': {
-    'browser': true, // enable use of global browser variables like `windows`.
-    'es6': true, // enable ES2015 features.
-    'node': true // enable use of global node variables like `process`.
+  env: {
+    browser: true, // enable use of global browser variables like `windows`.
+    es6: true, // enable ES2015 features.
+    node: true, // enable use of global node variables like `process`.
   },
-  'extends': [
+  extends: [
     'eslint:recommended', // Eslint recommended configuration by eslint.
     'plugin:import/recommended', // Linting of ES2015+ import/export syntax.
     'plugin:react/recommended', // Recommended react linting configs.
     'plugin:react-hooks/recommended', // Recommended react hooks linting configs.
     'plugin:jsx-a11y/recommended', // Turns on a11y rules for JSX.
     'plugin:@typescript-eslint/recommended', // Turns on rules from TypeScript-specific plugin.
-    'plugin:@typescript-eslint/recommended-requiring-type-checking' // Turns on rules from TypeScript-specific plugin.
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // Turns on rules from TypeScript-specific plugin.
   ],
-  'overrides': [
+  overrides: [
     {
-      'env': {
-        'jest/globals': true // enable Jest global variables.
+      env: {
+        'jest/globals': true, // enable Jest global variables.
       },
-      'extends': [
+      extends: [
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:jest-dom/recommended',
-        'plugin:testing-library/react'
+        'plugin:testing-library/react',
       ],
-      'files': [
+      files: [
         'src/**/__tests__/**/*.[jt]s?(x)',
-        'src/**/?(*.)+(spec|test).[jt]s?(x)'
+        'src/**/?(*.)+(spec|test).[jt]s?(x)',
       ], // Override config for same files pattern as jest `testMatch` default value
-      'plugins': [
-        'jest',
-        'jest-dom',
-        'testing-library'
-      ],
-      'rules': {
+      plugins: ['jest', 'jest-dom', 'testing-library'],
+      rules: {
         'jest-dom/prefer-checked': 'error',
         'jest-dom/prefer-enabled-disabled': 'error',
         'jest-dom/prefer-required': 'error',
@@ -46,68 +42,54 @@ module.exports = {
         'testing-library/await-async-query': 'error',
         'testing-library/no-await-sync-query': 'error',
         'testing-library/no-debugging-utils': 'warn',
-        'testing-library/no-dom-import': 'off'
-      }
-    }
-  ],
-  'parser': '@typescript-eslint/parser', // Allows Eslint to understand TypeScript syntax.
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true // enable jsx for React.
+        'testing-library/no-dom-import': 'off',
+      },
     },
-    'ecmaVersion': 2021, // ECMAScript version supported in the project.
-    'project': 'tsconfig.json', // Tells Eslint where to find the root tsconfig file of your project.
-    'sourceType': 'module' // set to `module` because we ue ECMAScript modules.
+  ],
+  parser: '@typescript-eslint/parser', // Allows Eslint to understand TypeScript syntax.
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // enable jsx for React.
+    },
+    ecmaVersion: 2021, // ECMAScript version supported in the project.
+    project: 'tsconfig.json', // Tells Eslint where to find the root tsconfig file of your project.
+    sourceType: 'module', // set to `module` because we ue ECMAScript modules.
   },
-  'plugins': [
+  plugins: [
     '@typescript-eslint',
     'import',
     'jsx-a11y',
     'react',
     'react-hooks',
-    'simple-import-sort' // Plugin for sorting imports in file.
+    'simple-import-sort', // Plugin for sorting imports in file.
   ],
-  'root': true,
-  'rules': {
+  root: true,
+  rules: {
     'import/first': 'error',
     'import/newline-after-import': 'error',
     'import/no-duplicates': 'error',
     'import/no-named-as-default-member': 'off',
     'import/no-unresolved': 'error',
-    'indent': [
-      'error',
-      2
-    ],
-    'no-multi-spaces': [
-      'error'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'quotes': ['error', 'single'],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-    'semi': [
-      'error',
-      'always'
-    ],
+    'semi': ['error', 'always'],
     'simple-import-sort/exports': 'error',
-    'simple-import-sort/imports': 'error'
+    'simple-import-sort/imports': 'error',
   },
-  'settings': {
+  settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': [
-        '.ts',
-        '.tsx'
-      ] // use typescript-eslint parser for .ts|tsx files.
+      '@typescript-eslint/parser': ['.ts', '.tsx'], // use typescript-eslint parser for .ts|tsx files.
     },
     'import/resolver': {
-      'typescript': {
-        'alwaysTryTypes': true // always try to resolve types under `<root>@types` directory even it doesn"t contain any source code, like `@types/unist`.
-      }
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn"t contain any source code, like `@types/unist`.
+      },
     },
-    'react': {
-      'version': 'detect' // auto-detect React version from package.json.
-    }
-  }
+    react: {
+      version: 'detect', // auto-detect React version from package.json.
+    },
+  },
 };
