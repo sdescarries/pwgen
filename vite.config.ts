2656c2145479c26c1ce6cdf07a018215a5dfe373
Simon Descarries <40470761+sdescarries@users.noreply.github.com>
2022-02-18 15:38:48 -0500

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import istanbul from 'vite-plugin-istanbul';
 import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/pwgen/',
   build: {
     sourcemap: true,
   },
@@ -18,6 +19,39 @@ export default defineConfig({
     istanbul({
       include: 'src/*',
       checkProd: true,
+    }),
+    VitePWA({
+
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png', 'pwgen.png'],
+      manifest: {
+        lang: 'en-us',
+        name: 'Secure Password Generator',
+        short_name: 'pwgen',
+        description: 'Secure Password Generator',
+        start_url: '/pwgen/',
+        background_color: '#161b1d',
+        theme_color: '#e2e7e9',
+        orientation: 'any',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/pwgen/favicon.svg',
+            sizes: '200x200',
+            type: 'image/svg',
+            purpose: 'any'
+          },
+          {
+            src: '/pwgen/apple-touch-icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwgen/pwgen.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
     legacy(),
     reactRefresh(),
