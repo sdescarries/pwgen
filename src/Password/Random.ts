067655d8706c41d31d271ce9899122ba21cd1e9e
Simon Descarries <40470761+sdescarries@users.noreply.github.com>
2026-03-02 09:42:41 -0500

    New: Add benchmarking feature

--- src/Password/Random.ts
+++ src/Password/Random.ts
@@ -1,17 +1,29 @@
 export class Random {
-  array = new Uint32Array(1024);
+  // Maximum amount of bytes is 0x10000
+  array = new Uint32Array(0x4000);
   index = 1024;
   range: number;
 
-  constructor(range: number) {
-    this.range = range;
+  constructor(range?: number) {
+    this.range = range ?? 0xFFFFFFFF;
   }
 
   pop(): number {
-    if (this.index >= 1024) {
+    if (this.index >= 0x4000) {
       crypto.getRandomValues(this.array);
       this.index = 0;
     }
     return this.array[this.index++] % this.range;
   }
+}
+
+export function benchmark() {
+  const beg = performance.now();
+  for (let n = 0; n < 0x10000; n++) {
+    new Random();
+  }
+  const end = performance.now() - beg;
+  const rat = 0x10000 * 0x10000 / end;
+
+  console.debug({ end, rat });
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from '@/App';
 import { reportWebVitals } from '@/reportWebVitals';
+import { benchmark } from '@/Password';
 
 const container = document.getElementById('root');
 
- 
 const root = createRoot(container!);
 root.render(<App />);
 
 reportWebVitals(console.log);
+
+Object.assign(window, { benchmark });
